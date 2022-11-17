7ea3693dc29492231e7676da6d69cc0179b75be1
SnehaRamisetti <115206730+SnehaRamisetti@users.noreply.github.com>
2022-11-17 10:57:25 +0530

    Add files via upload

---
+++ ADO/ADO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection("data source=.;database=sample;integrated security=True");
+            
+
+            SqlCommand cmd= new SqlCommand("Select * from EMP where ID=102", conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            
+            while (dr.Read())
+            {
+                string ID = dr["ID"].ToString();
+                string EMPName = dr["EMPName"].ToString();
+                string Salary = dr["Salary"].ToString();
+                Console.WriteLine(ID+""+EMPName+""+Salary);
+            }
+            
+            dr.Close();
+            
+            conn.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
